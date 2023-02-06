08542c8afe1947c2747bca587aa5c9c9d7fee475
Sasza44 <110614574+Sasza44@users.noreply.github.com>
2023-02-06 20:38:39 +0200

    Add files via upload

---
+++ JavaScript_13_1.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const button = document.querySelector('.currency-rates');
+button.addEventListener('click', updateCurrencyRates);
+function updateCurrencyRates(){
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            //console.log(xhr.responseText);
+
+            // шукаємо назви валют
+            let ind1 = [];
+            searchIndex(ind1, '"txt":"');
+            //console.log(ind1);
+
+            function searchIndex(index, substring){
+                let ind = 0;
+                let pos = 0;
+                while(ind != -1){
+                    ind = xhr.responseText.indexOf(substring, pos);
+                    index.push(ind);
+                    pos = ind + 1;
+                }
+                index.pop();
+            }
+
+            // шукаємо курси валют
+            let ind2 = [];
+            searchIndex(ind2, '","rate":');
+            //console.log(ind2);
+
+            // індекси, де закінчуються цифри
+            let ind3 = [];
+            searchIndex(ind3, ',"cc":"');
+            //console.log(ind2);
+
+            // виводимо назви валют
+            let names = [];
+            for(let i = 0; i < ind1.length; i++){
+                names[i] = xhr.responseText.substring(ind1[i] + 7, ind2[i]);
+            }
+            //console.log(names);
+
+            // виводимо курси валют
+            let rates = [];
+            for(let i = 0; i < ind1.length; i++){
+                rates[i] = xhr.responseText.substring(ind2[i] + 9, ind3[i]);
+            }
+            //console.log(rates);
+
+            // виводимо тільки ті валюти, у яких курс більший 25 грн.
+            let names1 = [];
+            let rates1 = [];
+            for(let i = 0; i < rates.length; i++){
+                if(rates[i] > 25){
+                    rates1.push(rates[i]);
+                    names1.push(names[i]);
+                }
+            }
+            //console.log(names1);
+            //console.log(rates1);
+            let rows = [];
+            let table = document.querySelector('table');
+            for(let i = 0; i < names1.length; i++){
+                rows[i] = table.insertRow(i);
+                rows[i].insertCell(0).innerHTML = names1[i];
+                rows[i].insertCell(1).innerHTML = rates1[i];
+            }
+        }
+    }
+
+    xhr.addEventListener('error', () => {
+        //console.log('error');
+        let err = document.createElement('div');
+        document.body.appendChild(err);
+        err.innerHTML = 'error';
+    });
+
+    xhr.open('GET', 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+    xhr.send();
+}
